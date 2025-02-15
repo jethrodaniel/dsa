24c3958243896e26a23dc360a9b61c596718f50b
Mark Delk <jethrodaniel@gmail.com>
2025-02-15 15:41:00 -0600

    refactor with #each

--- ruby/lib/dsa/single_node.rb
+++ ruby/lib/dsa/single_node.rb
@@ -12,20 +12,6 @@ module DSA
       @next = binding.local_variable_get(:next)
     end
 
-    # Append an element to the end of the list.
-    #
-    # Time: O(n), since we have to find the last node
-    # Space: O(1), no additional space based on input size
-    #
-    # @param value [T]
-    # @return [DSA::SingleNode] the new node
-    #
-    def append value
-      curr = self
-      curr = curr.next while curr.next
-      curr.next = DSA::SingleNode.new(value:)
-    end
-
     # Add an element to the beginning of the list.
     #
     # Time: O(1), since we don't have to iterate the list.
@@ -35,6 +21,76 @@ module DSA
     # @return [DSA::SingleNode] the new node
     #
     def prepend(value) = DSA::SingleNode.new(value:, next: self)
+
+    # Yield each element of the list.
+    #
+    # Time: O(n), since we iterate the list
+    # Space: O(1), no additional space based on input size
+    #
+    # @return [Enumerator<DSA::SingleNode>] if no block is given
+    #
+    def each
+      return to_enum(__method__) unless block_given?
+
+      curr = self
+
+      while curr.next
+        yield curr
+        curr = curr.next
+      end
+
+      yield curr
+    end
+
+    # Count how many items are in the list
+    #
+    # Time: O(n), since we have to iterate the list and count the items.
+    # Space: O(1), no additional space based on input size
+    #
+    # @return [Integer] the list length
+    #
+    def length = each.count
+
+    # Get the node at the given index
+    #
+    # Time: O(n), since we have to iterate the list to find the node at that index
+    # Space: O(1), no additional space based on input size
+    #
+    # @return [DSA::SingleNode] the node at that index
+    #
+    def [] index
+      raise ArgumentError, "index must be an integer" unless index.respond_to?(:to_i) && index.to_i == index
+      raise ArgumentError, "index must be 0 or greater" unless index >= 0
+
+      each.with_index do |item, i|
+        return item if i == index
+      end
+
+      raise ArgumentError, "index is too large"
+    end
+
+    # Delete an element
+    #
+    # Time: O(1), since we just have to iterate the list to find the node.
+    # Space: O(1), no additional space based on input size
+    #
+    # @param node [DSA::SingleNode] the node to delete
+    # @return [DSA::SingleNode] the deleted node
+    #
+    def delete node:
+      raise ArgumentError, "node must be a node" unless node.is_a?(DSA::SingleNode)
+
+      return node.next if self == node
+
+      each do |item|
+        if item.next == node
+          item.next = node.next
+          return node
+        end
+      end
+
+      raise ArgumentError, "node isn't in the list"
+    end
 
     # Insert an element at the given index.
     #
@@ -51,91 +107,25 @@ module DSA
 
       return DSA::SingleNode.new(value:, next: self) if index.zero?
 
-      curr = self
-      i = 1
-
-      while i < index && curr.next
-        curr = curr.next
-        i += 1
+      each.with_index do |item, i|
+        return item.next = DSA::SingleNode.new(value:, next: item.next) if i + 1 == index
       end
 
-      raise ArgumentError, "index is too large" if i < index
-
-      curr.next = DSA::SingleNode.new(value:, next: curr.next)
+      raise ArgumentError, "index is too large"
     end
 
-    # Delete an element
+    # Append an element to the end of the list.
     #
-    # Time: O(1), since we just have to iterate the list to find the node.
+    # Time: O(n), since we have to find the last node
     # Space: O(1), no additional space based on input size
     #
-    # @param node [DSA::SingleNode] the node to delete
-    # @return [DSA::SingleNode] the deleted node
+    # @param value [T]
+    # @return [DSA::SingleNode] the new node
     #
-    def delete node:
-      raise ArgumentError, "node must be a node" unless node.is_a?(DSA::SingleNode)
-
-      curr = self
-
-      return node.next if curr == node
-
-      while curr.next
-        if curr.next == node
-          curr.next = node.next
-          return node
-        end
-
-        curr = curr.next
-      end
-
-      raise ArgumentError, "node isn't in the list"
-    end
-
-    # Count how many items are in the list
-    #
-    # Time: O(n), since we have to iterate the list and count the items.
-    # Space: O(1), no additional space based on input size
-    #
-    # @return [Integer] the list length
-    #
-    def length
-      curr = self
-      num_items = 1
-
-      while curr.next
-        num_items += 1
-        curr = curr.next
-      end
-
-      num_items
-    end
-
-    # Get the node at the given index
-    #
-    # Time: O(n), since we have to iterate the list to find the node at that index
-    # Space: O(1), no additional space based on input size
-    #
-    # @return [DSA::SingleNode] the node at that index
-    #
-    def [] index
-      raise ArgumentError, "index must be an integer" unless index.respond_to?(:to_i) && index.to_i == index
-      raise ArgumentError, "index must be 0 or greater" unless index >= 0
-
-      return self if index.zero?
-
-      curr = self
-      i = 0
-
-      while curr.next
-        return curr if i == index
-
-        i += 1
-        curr = curr.next
-      end
-
-      raise ArgumentError, "index is too large" if i < index
-
-      curr
+    def append value
+      last = self
+      each { |item| last = item }
+      last.next = DSA::SingleNode.new(value:)
     end
   end
 end
--- ruby/spec/dsa/single_node_spec.rb
+++ ruby/spec/dsa/single_node_spec.rb
@@ -1,27 +1,4 @@
 RSpec.describe DSA::SingleNode do
-  describe "#append" do
-    let(:list) do
-      head = described_class.new(value: 1)
-      head.next = DSA::SingleNode.new(value: 2)
-      head
-    end
-
-    it "appends to the end of the list" do
-      head = list
-
-      expect(head.value).to eq 1
-      expect(head.next.value).to eq 2
-      expect(head.next.next).to be_nil
-
-      head.append 42
-
-      expect(head.value).to eq 1
-      expect(head.next.value).to eq 2
-      expect(head.next.next.value).to eq 42
-      expect(head.next.next.next).to be_nil
-    end
-  end
-
   describe "#prepend" do
     let(:list) do
       head = described_class.new(value: 1)
@@ -45,79 +22,102 @@ RSpec.describe DSA::SingleNode do
     end
   end
 
-  describe "#insert" do
+  describe "#each" do
+    let(:list) do
+      head = described_class.new(value: 1)
+      head.next = DSA::SingleNode.new(value: 2)
+      head
+    end
+
+    context "when given a block" do
+      it "calls the block for each element" do
+        items = []
+
+        list.each { |item| items << item.value } # rubocop:disable Style/MapIntoArray
+
+        expect(items).to eq [1, 2]
+      end
+    end
+
+    context "when not given a block" do
+      it "returns an enumerator" do
+        expect(list.each).to be_an_instance_of(Enumerator)
+      end
+    end
+  end
+
+  describe "#length" do
+    let(:list) do
+      head = described_class.new(value: 1)
+      head.next = DSA::SingleNode.new(value: 2)
+      head
+    end
+
+    it "returns the number of items in the list" do
+      expect(list.length).to eq 2
+    end
+  end
+
+  describe "#[]" do
     let(:list) { described_class.new(value: 1) }
 
     context "when index is invalid" do
       it "errors" do
-        expect { list.insert(value: 42, index: nil) }
-          .to raise_error(ArgumentError, "index must be an integer")
+        expect { list[nil] }.to raise_error(ArgumentError, "index must be an integer")
       end
     end
 
     context "when index is negative" do
       it "errors" do
-        expect { list.insert(value: 42, index: -1) }
-          .to raise_error(ArgumentError, "index must be 0 or greater")
+        expect { list[-1] }.to raise_error(ArgumentError, "index must be 0 or greater")
       end
     end
 
     context "when index is too large" do
       it "errors" do
-        expect { list.insert(value: 42, index: 2) }
-          .to raise_error(ArgumentError, "index is too large")
+        expect { list[42] }.to raise_error(ArgumentError, "index is too large")
       end
     end
 
     context "when index is 0" do
-      it "inserts at the beginning of the list" do
+      it "returns the first element" do
         head = list
 
         expect(head.value).to eq 1
         expect(head.next).to be_nil
 
-        new_node = head.insert value: 42, index: 0
-        expect(new_node).to be_an_instance_of(DSA::SingleNode)
-
-        head = new_node
-        expect(head.value).to eq 42
-        expect(head.next.value).to eq 1
-        expect(head.next.next).to be_nil
+        node = head[0]
+        expect(node).to eq(head)
       end
     end
 
-    context "when index is the length of the list" do
-      it "inserts at the end of the list" do
+    context "when index is the length of the list - 1" do
+      it "returns the last element" do
         head = list
-
-        expect(head.value).to eq 1
-        expect(head.next).to be_nil
-
-        new_node = head.insert value: 42, index: 1
-        expect(new_node).to be_an_instance_of(DSA::SingleNode)
-
-        expect(head.value).to eq 1
-        expect(head.next.value).to eq 42
-        expect(head.next.next).to be_nil
-      end
-    end
-
-    context "when index is in the middle" do
-      it "inserts in the middle of the list" do
-        head = list
-        head.next = described_class.new(value: 2)
+        head.next = tail = described_class.new(value: 2)
 
         expect(head.value).to eq 1
         expect(head.next.value).to eq 2
         expect(head.next.next).to be_nil
 
-        new_node = head.insert value: 42, index: 1
-        expect(new_node).to be_an_instance_of(DSA::SingleNode)
+        node = head[1]
+        expect(node).to eq(tail)
+      end
+    end
+
+    context "when index is in the middle" do
+      it "returns the middle element" do
+        head = list
+        head.next = middle = described_class.new(value: 2)
+        middle.next = described_class.new(value: 3)
 
         expect(head.value).to eq 1
-        expect(head.next.value).to eq 42
-        expect(head.next.next.value).to eq 2
+        expect(head.next.value).to eq 2
+        expect(head.next.next.value).to eq 3
         expect(head.next.next.next).to be_nil
+
+        node = head[1]
+        expect(node).to eq(middle)
       end
     end
   end
@@ -204,79 +204,103 @@ RSpec.describe DSA::SingleNode do
     end
   end
 
-  describe "#length" do
+  describe "#insert" do
+    let(:list) { described_class.new(value: 1) }
+
+    context "when index is invalid" do
+      it "errors" do
+        expect { list.insert(value: 42, index: nil) }
+          .to raise_error(ArgumentError, "index must be an integer")
+      end
+    end
+
+    context "when index is negative" do
+      it "errors" do
+        expect { list.insert(value: 42, index: -1) }
+          .to raise_error(ArgumentError, "index must be 0 or greater")
+      end
+    end
+
+    context "when index is too large" do
+      it "errors" do
+        expect { list.insert(value: 42, index: 2) }
+          .to raise_error(ArgumentError, "index is too large")
+      end
+    end
+
+    context "when index is 0" do
+      it "inserts at the beginning of the list" do
+        head = list
+
+        expect(head.value).to eq 1
+        expect(head.next).to be_nil
+
+        new_node = head.insert value: 42, index: 0
+        expect(new_node).to be_an_instance_of(DSA::SingleNode)
+
+        head = new_node
+        expect(head.value).to eq 42
+        expect(head.next.value).to eq 1
+        expect(head.next.next).to be_nil
+      end
+    end
+
+    context "when index is the length of the list" do
+      it "inserts at the end of the list" do
+        head = list
+
+        expect(head.value).to eq 1
+        expect(head.next).to be_nil
+
+        new_node = head.insert value: 42, index: 1
+        expect(new_node).to be_an_instance_of(DSA::SingleNode)
+
+        expect(head.value).to eq 1
+        expect(head.next.value).to eq 42
+        expect(head.next.next).to be_nil
+      end
+    end
+
+    context "when index is in the middle" do
+      it "inserts in the middle of the list" do
+        head = list
+        head.next = described_class.new(value: 2)
+
+        expect(head.value).to eq 1
+        expect(head.next.value).to eq 2
+        expect(head.next.next).to be_nil
+
+        new_node = head.insert value: 42, index: 1
+        expect(new_node).to be_an_instance_of(DSA::SingleNode)
+
+        expect(head.value).to eq 1
+        expect(head.next.value).to eq 42
+        expect(head.next.next.value).to eq 2
+        expect(head.next.next.next).to be_nil
+      end
+    end
+  end
+
+  describe "#append" do
     let(:list) do
       head = described_class.new(value: 1)
       head.next = DSA::SingleNode.new(value: 2)
       head
     end
 
-    it "returns the number of items in the list" do
-      expect(list.length).to eq 2
-    end
-  end
+    it "appends to the end of the list" do
+      head = list
 
-  describe "#[]" do
-    let(:list) { described_class.new(value: 1) }
+      expect(head.value).to eq 1
+      expect(head.next.value).to eq 2
+      expect(head.next.next).to be_nil
 
-    context "when index is invalid" do
-      it "errors" do
-        expect { list[nil] }.to raise_error(ArgumentError, "index must be an integer")
-      end
-    end
+      head.append 42
 
-    context "when index is negative" do
-      it "errors" do
-        expect { list[-1] }.to raise_error(ArgumentError, "index must be 0 or greater")
-      end
-    end
-
-    context "when index is too large" do
-      it "errors" do
-        expect { list[42] }.to raise_error(ArgumentError, "index is too large")
-      end
-    end
-
-    context "when index is 0" do
-      it "returns the first element" do
-        head = list
-
-        expect(head.value).to eq 1
-        expect(head.next).to be_nil
-
-        node = head[0]
-        expect(node).to eq(head)
-      end
-    end
-
-    context "when index is the length of the list - 1" do
-      it "returns the last element" do
-        head = list
-        head.next = tail = described_class.new(value: 2)
-
-        expect(head.value).to eq 1
-        expect(head.next.value).to eq 2
-        expect(head.next.next).to be_nil
-
-        node = head[1]
-        expect(node).to eq(tail)
-      end
-    end
-
-    context "when index is in the middle" do
-      it "returns the middle element" do
-        head = list
-        head.next = middle = described_class.new(value: 2)
-        middle.next = described_class.new(value: 3)
-
-        expect(head.value).to eq 1
-        expect(head.next.value).to eq 2
-        expect(head.next.next.value).to eq 3
-        expect(head.next.next.next).to be_nil
-
-        node = head[1]
-        expect(node).to eq(middle)
-      end
+      expect(head.value).to eq 1
+      expect(head.next.value).to eq 2
+      expect(head.next.next.value).to eq 42
+      expect(head.next.next.next).to be_nil
     end
   end
 end
