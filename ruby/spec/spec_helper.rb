f0df37041c123c96caca21fd4acfdfcfe60f7002
Mark Delk <jethrodaniel@gmail.com>
2025-02-15 16:34:55 -0600

    add simplecov

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -1,3 +1,12 @@
+require "simplecov"
+
+SimpleCov.start do
+  enable_coverage :branch
+  enable_coverage_for_eval
+
+  minimum_coverage 100
+end
+
 require "dsa"
 
 RSpec.configure(&:disable_monkey_patching!)
