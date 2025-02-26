7b47393eb36597eec476155ca77399bf7214668d
Mark Delk <jethrodaniel@gmail.com>
2025-02-25 22:25:15 -0600

    stack complexity specs

--- ruby/spec/dsa/stack_spec.rb
+++ ruby/spec/dsa/stack_spec.rb
@@ -1,4 +1,13 @@
 RSpec.describe DSA::Stack do
+  def generate_stacks from, to
+    bench_range(from, to).map do |n|
+      stack = described_class.new
+      1.upto(n).each { stack.push _1 }
+      stack
+    end
+  end
+
+
   describe "#push" do
     it "pushes an element to the top of the stack" do
       stack = described_class.new
@@ -8,6 +17,16 @@ RSpec.describe DSA::Stack do
 
       expect(stack.pop).to eq 2
       expect(stack.pop).to eq 1
+    end
+
+    it "has O(1) time complexity" do
+      stacks = generate_stacks 1, 1_000
+      expect { |n, i| stacks[i].push 42 }.to perform_constant
+    end
+
+    it "has O(1) space complexity" do
+      stack = described_class.new
+      expect { stack.push 42 }.to perform_allocation(3).and_retain(3)
     end
   end
 
@@ -29,6 +48,16 @@ RSpec.describe DSA::Stack do
         expect(stack.pop).to eq 1
       end
     end
+
+    it "has O(1) time complexity" do
+      stacks = generate_stacks 10, 1_000
+      expect { |_, i| stacks[i].pop }.to perform_constant.in_range(10, 1_000)
+    end
+
+    it "has O(1) space complexity" do
+      stack = generate_stacks(1, 1).first
+      expect { stack.push 42 }.to perform_allocation(3).and_retain(3)
+    end
   end
 
   describe "#peek" do
@@ -47,6 +76,16 @@ RSpec.describe DSA::Stack do
         expect(stack.peek).to eq 1
         expect(stack.peek).to eq 1
       end
+    end
+
+    it "has O(1) time complexity" do
+      stacks = generate_stacks 1, 1_000
+      expect { |_, i| stacks[i].peek }.to perform_constant
+    end
+
+    it "has O(1) space complexity" do
+      stack = generate_stacks(1, 1).first
+      expect { stack.push 42 }.to perform_allocation(3).and_retain(3)
     end
   end
 
@@ -70,6 +109,16 @@ RSpec.describe DSA::Stack do
         expect(stack.empty?).to be true
       end
     end
+
+    it "has O(1) time complexity" do
+      stacks = generate_stacks 1, 1_000
+      expect { |_, i| stacks[i].empty? }.to perform_constant
+    end
+
+    it "has O(1) space complexity" do
+      stack = generate_stacks(1, 1).first
+      expect { stack.empty? }.to perform_allocation(0)
+    end
   end
 
   describe "#length" do
@@ -85,6 +134,17 @@ RSpec.describe DSA::Stack do
       expect(stack.length).to eq 1
       stack.pop
       expect(stack.length).to eq 0
+    end
+
+    it "has O(n) time complexity" do
+      stacks = generate_stacks 1, 1_000
+      expect { |_, i| stacks[i].length }.to perform_linear.in_range(1, 1_000)
+    end
+
+    it "has O(1) space complexity" do
+      stack = described_class.new
+      1.upto(1_000).each { stack.push _1 }
+      expect { stack.length }.to perform_allocation(1).and_retain(1)
     end
   end
 
