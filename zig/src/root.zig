13eddefa93e6a4749e468d531416905c01c9e333
Mark Delk <jethrodaniel@gmail.com>
2025-03-01 11:27:51 -0600

    fix missing zig documentation for sorting

--- zig/src/root.zig
+++ zig/src/root.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub const SinglyLinkedList = @import("./singly_linked_list.zig");
+pub const Sorting = @import("./sorting.zig");
 
 test {
     std.testing.refAllDecls(@This());
